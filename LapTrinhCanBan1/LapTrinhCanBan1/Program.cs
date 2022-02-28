cdf618599d6d618a4f914a7aba89ebb415cb3c55
namdongvando <32663464+namdongvando@users.noreply.github.com>
2022-02-28 21:27:46 +0700

    Buoi1

---
+++ LapTrinhCanBan1/LapTrinhCanBan1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LapTrinhCanBan1
+{
+    class Program
+    {
+        /// <summary>
+        /// ham nay dung de lam gi
+        /// </summary>
+        /// <param name="args"> tham so cua no ra sao</param>
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World 1");
+            // Console.ReadKey();
+            string ten = Console.ReadLine();
+            int tuoi = 20;
+            long tuoi1 = 100;
+            double diem = 10.4;
+            float diem11 = 92;
+            char abcchar = 'a';
+            var dungChoThangNaoCungDc = "asdasda";
+            Console.WriteLine(tuoi);
+            Console.WriteLine(tuoi1);
+            Console.WriteLine(diem);
+            Console.WriteLine(abcchar);
+            Console.WriteLine(dungChoThangNaoCungDc);
+            Console.WriteLine(diem11);
+        }
+    }
+}
---
+++ LapTrinhCanBan1/ToanTu/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ToanTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 5;
+                int b = 4;
+                // xuất ra màn hình
+                Console.WriteLine("Nhap a:");
+                // nhap tu ban phim so a
+                string inputA = Console.ReadLine();
+                //a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhap b:");
+                // nhap tu ban phim so b
+                string inputB = Console.ReadLine();
+                //b = int.Parse(Console.ReadLine());
+
+                a = int.Parse(inputA);
+                b = int.Parse(inputB);
+                //int.TryParse(inputA, out a);
+                //int.TryParse(inputB, out b); 
+                Console.WriteLine("tong cua a + b la {0} "
+                    , a + b);
+                Console.WriteLine("tong cua {0} + {1} la {2} "
+                    , a, b, a + b);
+                Console.WriteLine("hieu cua {0} - {1} la {2} "
+                    , a, b, a - b);
+                Console.WriteLine("tich cua {0} * {1} la {2} "
+                    , a, b, a * b);
+                Console.WriteLine("thuong cua {0} / {1} la {2} "
+                    , a, b, a / b);
+                Console.WriteLine("chia lay du cua {0}%2 la {1} "
+                    , a, a % 2);
+            }
+            catch (Exception ex)
+            {
+                Main(args);
+            }
+
+           
+
+
+        }
+    }
+}
