d8229874d3d99cdd27b83534e4649c199706f16e
namdongvando <32663464+namdongvando@users.noreply.github.com>
2022-03-21 18:32:16 +0700

    PhuongTrinh

---
+++ LapTrinhCanBan1/PhuongTrinh/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace PhuongTrinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSoA = new System.Windows.Forms.TextBox();
+            this.txtSoB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGIai = new System.Windows.Forms.Button();
+            this.btnLamLai = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SO A";
+            // 
+            // txtSoA
+            // 
+            this.txtSoA.Location = new System.Drawing.Point(215, 127);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(317, 20);
+            this.txtSoA.TabIndex = 1;
+            // 
+            // txtSoB
+            // 
+            this.txtSoB.Location = new System.Drawing.Point(215, 165);
+            this.txtSoB.Name = "txtSoB";
+            this.txtSoB.Size = new System.Drawing.Size(317, 20);
+            this.txtSoB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "So B";
+            // 
+            // txtSoC
+            // 
+            this.txtSoC.Location = new System.Drawing.Point(215, 205);
+            this.txtSoC.Name = "txtSoC";
+            this.txtSoC.Size = new System.Drawing.Size(317, 20);
+            this.txtSoC.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "So C";
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(226, 326);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(317, 20);
+            this.txtKetQua.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 329);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Kết Quả";
+            // 
+            // btnGIai
+            // 
+            this.btnGIai.Location = new System.Drawing.Point(215, 262);
+            this.btnGIai.Name = "btnGIai";
+            this.btnGIai.Size = new System.Drawing.Size(75, 23);
+            this.btnGIai.TabIndex = 8;
+            this.btnGIai.Text = "Giải";
+            this.btnGIai.UseVisualStyleBackColor = true;
+            this.btnGIai.Click += new System.EventHandler(this.btnGIai_Click);
+            // 
+            // btnLamLai
+            // 
+            this.btnLamLai.Location = new System.Drawing.Point(341, 262);
+            this.btnLamLai.Name = "btnLamLai";
+            this.btnLamLai.Size = new System.Drawing.Size(75, 23);
+            this.btnLamLai.TabIndex = 9;
+            this.btnLamLai.Text = "Làm Lại";
+            this.btnLamLai.UseVisualStyleBackColor = true;
+            this.btnLamLai.Click += new System.EventHandler(this.btnLamLai_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(457, 262);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(302, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Phương Trinh Bac 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLamLai);
+            this.Controls.Add(this.btnGIai);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSoC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSoA;
+        private System.Windows.Forms.TextBox txtSoB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGIai;
+        private System.Windows.Forms.Button btnLamLai;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ LapTrinhCanBan1/PhuongTrinh/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhuongTrinh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGIai_Click(object sender, EventArgs e)
+        {
+            // kiểm tra input
+            ChekInput();
+            double a, b, c;
+            double.TryParse(txtSoA.Text,out a);
+            double.TryParse(txtSoB.Text, out b);
+            double.TryParse(txtSoC.Text, out c);
+            txtKetQua.Text = GiaiPhuongTrinhB2(a, b, c);
+        }
+
+        private string GiaiPhuongTrinhB2(double a, double b, double c)
+        {
+            try
+            {
+                double d = Math.Pow(b, 2) - 4 * a * c;
+                if (d < 0)
+                {
+                    throw new Exception("PT Vo Nghiệm");
+                }
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+
+                return string.Format("X1={0:F2}, X2={1:F2}",x1,x2);
+            }
+            catch (Exception ex) 
+            {
+                return ex.Message;
+            }
+            
+        }
+
+        /// <summary>
+        /// kiểm tra input
+        /// </summary>
+        private void ChekInput()
+        {
+            try
+            {
+                if (txtSoA.Text == "")
+                {
+                    txtSoA.Focus();
+                    txtSoA.SelectAll();
+                    throw new Exception("Bạn chưa Nhập Số A");
+                }
+                if (txtSoB.Text == "")
+                {
+                    txtSoB.Focus();
+                    txtSoB.SelectAll();
+                    throw new Exception("Bạn chưa Nhập Số B");
+                }
+                if (txtSoC.Text == "")
+                {
+                    txtSoC.Focus();
+                    txtSoC.SelectAll();
+                    throw new Exception("Bạn chưa Nhập Số C");
+                }
+                double a, b, c;
+                if (double.TryParse(txtSoA.Text, out a) ==false){
+                    txtSoA.Focus();
+                    txtSoA.SelectAll();
+                    throw new Exception("So A Không Hợp Lệ");
+
+                }
+                if (double.TryParse(txtSoB.Text, out b) == false)
+                {
+                    txtSoB.Focus();
+                    txtSoB.SelectAll();
+                    throw new Exception("So B Không Hợp Lệ");
+
+                }
+                if (double.TryParse(txtSoC.Text, out c) == false)
+                {
+                    txtSoC.Focus();
+                    txtSoC.SelectAll();
+                    throw new Exception("So C Không Hợp Lệ");
+
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+             
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLamLai_Click(object sender, EventArgs e)
+        {
+            txtSoA.Text = "";
+            txtSoB.Text = "";
+            txtSoC.Text = "";
+            txtKetQua.Text = "";
+        }
+    }
+}
